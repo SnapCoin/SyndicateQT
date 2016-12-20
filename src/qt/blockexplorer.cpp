ed8220b299c0e93963acbd89392eb280bade6d58
SnapCoin <snapcoin@mail.com>
2016-12-20 09:57:49 +0100

    Rename website url

--- src/qt/blockexplorer.cpp
+++ src/qt/blockexplorer.cpp
@@ -6,7 +6,7 @@ BlockExplorer::BlockExplorer(QWidget *parent) :
     ui(new Ui::BlockExplorer)
 {
     ui->setupUi(this);
-    ui->webView->load(QUrl("http://blockexplorer.SnapCoinlabs.org"));
+    ui->webView->load(QUrl("http://blockexplorer.snapcoin.online"));
 }
 
 BlockExplorer::~BlockExplorer()
